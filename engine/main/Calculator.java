d73da979f77612dede66e392d9431ecdc48814bb
cammori <cammori@gmail.com>
2015-04-11 13:47:38 -0400

    multiplies!

--- engine/main/Calculator.java
+++ engine/main/Calculator.java
@@ -8,17 +8,19 @@ public class Calculator implements CalculateThis {
 		super();
 	}
 
-	public Double CalculateThisSetOfStuff(List<Double> inputs, Operations operation) throws Exception {
+	public Double CalculateThisSetOfStuff(List<Double> inputs,
+			Operations operation) throws Exception {
 		if (inputs.size() != 2) {
 			throw new Exception();
 		}
 		switch (operation) {
 		case ADD:
 			return inputs.get(0) + inputs.get(1);
+		case MULTIPLY:
+			return inputs.get(0) * inputs.get(1);
 		default:
-			break;
+			break; 
 		}
-			
 		return 0.0;
 	}
 
